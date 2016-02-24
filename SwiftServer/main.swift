52a39dfd05d3ad01a21ccf37e2acea634263b701
Elliot T <tikhomirovi@icloud.com>
2016-02-24 16:53:53 -0500

    Initial Commit

---
+++ SwiftServer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftServer
+//
+//  Created by Ilia Tikhomirov on 24/02/16.
+//  Copyright © 2016 Ilia Tikhomirov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
